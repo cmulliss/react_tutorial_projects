10c4454a476b91729e30f4fde3fb1e8ac5c1b307
cmulliss <cmulliss@gmail.com>
2022-05-20 18:14:42 +0100

    adding some code

---
+++ tutorial/src/components/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import Greeting from './Greeting'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>App</h1>
+      <button>click</button>
+      <Greeting />
+    </div>
+  )
+}
+
+export default App
